2d8140840d9daa882baca593466062e4f217d882
fishbnik <fishbnik@gmail.com>
2026-06-18 14:01:21 +0200

    Aplikace si pamatuje a po spušténí načte posledně vybranou cestu k protokolům

--- Stitky/SettingsPage.xaml.cs
+++ Stitky/SettingsPage.xaml.cs
@@ -6,17 +6,29 @@ namespace Stitky
         public SettingsPage()
         {
             InitializeComponent();
+
+            SlozkaProtokolu.Text =
+                  Preferences.Get("SlozkaProtokolu", "");
+
         }
 
         private async void VyberSlozkyProtokolu(object sender, EventArgs e)
         {
             string VybranaSlozka = string.Empty;
             var result = await FolderPicker.Default.PickAsync();
-
+            
+            // Výběr složky protokolů a uložení cesty do nastavení aplikace
+            
             if (result.IsSuccessful)
             {
                 VybranaSlozka = result.Folder.Path;
                 SlozkaProtokolu.Text = VybranaSlozka;
+
+                //Uložení cesty do nastavení aplikace
+                Preferences.Set(
+                            "SlozkaProtokolu",
+                            result.Folder.Path);
+
             }
             else
             {
